a382bc7187ed5844d217a72c977e06a0b45d86ca
Lucho07 <luis.1234albert@gmail.com>
2026-05-10 22:40:23 -0500

    Configurar diferente puerto para el envío de emails

--- config/mailer.js
+++ config/mailer.js
@@ -2,12 +2,15 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-    port: process.env.EMAIL_PORT || 587,
-    secure: false, // true para 465, false para otros
+    port: parseInt(process.env.EMAIL_PORT) || 465,
+    secure: process.env.EMAIL_PORT ? parseInt(process.env.EMAIL_PORT) === 465 : true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,  // 10s máximo para conectar
+    greetingTimeout: 10000,
+    socketTimeout: 15000
 });
 
 module.exports = transporter;
